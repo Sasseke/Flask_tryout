f1d54ba67eaf27dc73d0360dc217be6a2d24db26
basic Flask code <sasseku@gmail.com>
2023-10-21 13:49:37 +0200

    Updated 21 oktober 2023

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
+
+app=Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
 
 @app.route('/')
 def home():
